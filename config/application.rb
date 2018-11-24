db56a83db686d8b8833908864e480cffca8f9c51
Tristin Baker <tristinbaker52@gmail.com>
2018-11-23 20:05:33 -0500

    please

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Website
     config.load_defaults 5.2
     #config.assets.initialize_on_precompile = true
     config.serve_static_assets = true
-    config.assets.precompile += %w[ '*.js', '*.css', '*.scss' ]
+    # config.assets.precompile += %w[ '*.js', '*.css', '*.scss' ]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
