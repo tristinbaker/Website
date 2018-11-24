b96ff7517703160a2f12042f8e06b8f592cc4182
Tristin Baker <tristinbaker52@gmail.com>
2018-11-24 13:01:16 -0500

    check jquery

--- app/assets/javascripts/vim_commands.js
+++ app/assets/javascripts/vim_commands.js
@@ -7,3 +7,13 @@ $(document).ready(function() {
   });
 });
 
+
+window.onload = function() {
+    if (window.jQuery) {  
+        // jQuery is loaded  
+        alert("Yeah!");
+    } else {
+        // jQuery is not loaded
+        alert("Doesn't Work");
+    }
+}
