c3d93ba992b20e65cac2144e8daf94743a79a578
Tristin Baker <tristinbaker52@gmail.com>
2018-11-24 16:33:02 -0500

    add copy functionality

--- app/assets/javascripts/vim_command.js
+++ app/assets/javascripts/vim_command.js
@@ -6,3 +6,20 @@ $(document).ready(function() {
     });
   });
 });
+
+function commandCopy(id) {
+  var div = '#statement' + id;
+  var input = document.createElement('input');
+  document.body.appendChild(input);
+  input.value = $(div).text();
+  input.select();
+  document.execCommand('copy');
+  input.remove();
+  launch_toast();
+}
+
+function launch_toast() {
+  var x = document.getElementById("toast");
+  x.className = "show";
+  setTimeout(function() { x.className = x.className.replace("show", ""); }, 5000);
+}
