9fda54e9552c953b1807b32de23e30f8b6957bd1
Tristin Baker <tristinbaker52@gmail.com>
2018-11-24 19:54:50 -0500

    add gifs to vimrcs

--- app/controllers/vimrcs_controller.rb
+++ app/controllers/vimrcs_controller.rb
@@ -6,11 +6,22 @@ class VimrcsController < ApplicationController
 
   def index; end
 
-  def show; end
+  def show
+    @vimrc = Vimrc.find(params[:id])
+  end
 
-  def new; end
+  def new
+    @vimrc = Vimrc.new
+  end
 
-  def create; end
+  def create
+    @vimrc = Vimrc.new(vimrc_params)
+    if @vimrc.save
+      redirect_to vimrcs_path
+    else
+      redirect_to vim_commands_path
+    end
+  end
 
   def edit; end
 
@@ -22,6 +33,10 @@ class VimrcsController < ApplicationController
 
   def set_vim_types
     @vim_types = VimType.exclude_non_vimrc.eager_load(:vimrcs)
+  end
+
+  def vimrc_params
+    params.require(:vimrc).permit(:function, :comments, :vim_type_id)
   end
 
 end
