3cc057826732c2beb7acc9ae2c01d8cb0c4fc531
Tristin Baker <tristinbaker52@gmail.com>
2019-01-01 14:08:48 -0500

    add comments

---
+++ db/migrate/20190101190838_create_comments.rb
@@ -0,0 +1,9 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.string :name
+      t.text :content
+      t.references :post, foreign_key: true
+    end
+  end
+end
